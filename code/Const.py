7b142f0b4f3989d0f30e524f5592ed701a4d3f2a
WendersonPereiraLopes <wendersonlopes.softeng@gmail.com>
2026-06-13 15:41:31 -0300

    add IMG = ICON and update de code.

--- code/Const.py
+++ code/Const.py
@@ -1,7 +1,10 @@
+import pygame.image
+
 #C
 C_WHITE = (255, 255, 255)
 C_YELLOW = (255,255, 0)
-
+#I
+ICON = pygame.image.load('asset/IconBg.png')
 #M
 MENU_OPTION = (
                 'New Game',
--- code/Game.py
+++ code/Game.py
@@ -1,13 +1,14 @@
 import pygame
 
 from code.Menu import Menu
-from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Const import WIN_WIDTH, WIN_HEIGHT, ICON
 
 class Game:
 
     def __init__(self):
         pygame.init()
         self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
+        self.icon = pygame.display.set_icon(ICON)
         self.title = pygame.display.set_caption('Crystal Legacy') # Altera o Título da Janela
 
     def run(self):
