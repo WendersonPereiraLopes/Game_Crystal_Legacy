2c06b6b08aa3148aa012c836b4642075d84f6311
WendersonPereiraLopes <wendersonlopes.softeng@gmail.com>
2026-06-13 13:22:31 -0300

    Create Menu and Title the Game

---
+++ code/Const.py
@@ -0,0 +1,14 @@
+#C
+C_WHITE = (255, 255, 255)
+C_YELLOW = (255,255, 0)
+
+#M
+MENU_OPTION = (
+                'New Game',
+                'Load Game',
+                'Exit'
+)
+
+#W
+WIN_WIDTH = 800
+WIN_HEIGHT = 456
